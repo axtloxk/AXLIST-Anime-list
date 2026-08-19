f9f5d7efcedb7cebea8902471bc1d08927e4778a
axtloxk <axtloxkk@email.com>
2026-08-19 20:07:15 +0200

    changed the height and width for the loading state for the my-list

--- app/my-list/loading.tsx
+++ app/my-list/loading.tsx
@@ -1,16 +1,16 @@
 export default function Loading() {
   return (
-    <div className="container min-h-screen px-6 mt-8 py-4 lg:min-w-full lg:px-14 lg:py-0 animate-pulse">
+    <div className="container min-h-screen px-6 mt-5 py-4 lg:min-w-full lg:px-14 lg:py-0 animate-pulse">
       {/* Header Skeleton */}
       <div className="flex items-center gap-4">
         {/* Back Button Placeholder */}
-        <div className="-ml-2.5 h-8 w-8 rounded-md bg-zinc-800/80 lg:ml-0" />
+        <div className="-ml-2.5 h-6 w-8 rounded-md bg-zinc-800/80 lg:ml-0" />
         {/* Page Title Placeholder */}
-        <div className="h-8 w-44 rounded-md bg-zinc-800/80" />
+        <div className="h-6 w-40 rounded-md bg-zinc-800/80" />
       </div>
 
       {/* Separator Skeleton */}
-      <div className="mt-3 mb-5 h-px w-full bg-zinc-800/60" />
+      <div className="mt-7 mb-7 h-px w-full bg-zinc-800/60" />
 
       {/* Anime Grid Skeleton */}
       <div className="grid grid-cols-2 gap-6 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
